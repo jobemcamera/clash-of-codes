10321db1eb7b8c9e6f0316e3710c75bdb4d23791
jobemcamera <jobecamera@gmail.com>
2023-02-19 10:38:24 -0300

    The game mode is REVERSE: multiplicacao,soma,subtracao

---
+++ 2023-fevereiro/js/20230219.js
@@ -0,0 +1,40 @@
+/*
+The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
+
+Test 1
+    Input
+    8 2
+    Expected output
+    16106
+
+Test 2
+    Input
+    5 4
+    Expected output
+    2091
+
+Test 3
+    Input
+    9 6
+    Expected output
+    54153
+
+Test 4
+    Input
+    20 3
+    Expected output
+    602317
+*/
+
+var inputs = readline().split(' ');
+const a = parseInt(inputs[0]);
+const b = parseInt(inputs[1]);
+
+const multiplicacao = a*b;
+const soma = a+b;
+const subtracao = a-b;
+
+let lista = [];
+
+lista.push(multiplicacao, soma, subtracao)
+console.log(lista.join(''))
