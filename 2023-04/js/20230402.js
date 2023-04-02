c03e26c32e6593de2f766d38a9ef6e571bd7cbb8
jobemcamera <jobecamera@gmail.com>
2023-04-02 11:33:31 -0300

    the least number of socks he should extract so Bob could garantee that he have at least 2 socks of the same color

---
+++ 2023-04/js/20230402.js
@@ -0,0 +1,24 @@
+/*
+Bob has in his SockBox Multiple pairs of socks with n different colors .
+the pairs are separated and distributed randomly on the box .
+Bob can't see the colors of the socks because the room is dark and he's is in a hurry .
+for a match of football he only need 2 socks with the same color .
+given the number n of the different sock colors he have ....
+output the least amount of socks he should take out from the box so he could have at least 2 socks of the same color .
+
+Input
+n the total number of different colors of his all sock pairs in the box .
+Output
+the least number of socks he should extract so he could garantee that he have at least 2 socks of the same color .
+
+Constraints
+Example
+Input
+2
+
+Output
+3
+*/
+
+const n = parseInt(readline());
+console.log(n+1);
